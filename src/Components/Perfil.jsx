a161ca139a03f940dc999893d8188255383c73e9
Extiben <jhon.mosquerapalacios@inemjose.edu.co>
2025-06-10 14:13:45 -0500

    feat: componente Perfil dinámico con props

--- src/Components/Perfil.jsx
+++ src/Components/Perfil.jsx
@@ -1,13 +1,9 @@
-export default function Perfil() {
+function Perfil({ resumen }) {
   return (
     <section>
-      <h2>Perfil</h2>
-      <p>
-        Tecnólogo en Análisis y Desarrollo de Software con experiencia en desarrollo web y móvil. 
-        Apasionado por la tecnología, con habilidades en lenguajes como Python, javascript, php, Java y bases de datos 
-        y un alto compromiso con la puntualidad en la entrega de proyectos.
-        Conocimientos varios en Ciberseguridad y hackingwifi
-      </p>
+      <h3>Perfil Profesional</h3>
+      <p>{resumen}</p>
     </section>
   );
 }
+export default Perfil;
