6ff2c4255af2822fa580d2615de61351586996cc
Extiben <jhon.mosquerapalacios@inemjose.edu.co>
2025-06-10 15:01:12 -0500

    feat: Experiencia mapeada desde arreglo en App.jsx

--- src/Components/Experiencia.jsx
+++ src/Components/Experiencia.jsx
@@ -1,25 +1,15 @@
-export default function Experiencia() {
+function Experiencia({ experiencias }) {
   return (
     <section>
-      <h3>Experiencia Profesional</h3>
+      <h3>Experiencia Laboral</h3>
       <ul>
-        <li>
-          <strong>Desarrollador Fullstack</strong> – Inemsite (2022–2024)
-          <p>Diseño e implementación de interfaces web responsivas usando React y Tailwindcss. Integración con APIs REST y optimización del rendimiento.</p>
-        </li>
-        <li>
-          <strong>Instructor TIC</strong> – SENA (2020–2022)
-          <p>Formación en desarrollo web y programación básica. Asesoría en proyectos académicos y soporte técnico en plataformas educativas.</p>
-        </li>
-        <li>
-          <strong>Desarrollador Full Stack</strong> – Proyecto Freelance (2025)
-          <p>Creación de una plataforma de gestión de hojas de vida con Node.js y React. Implementación de login, generación de PDFs y conexión a base de datos MongoDB.</p>
-        </li>
-        <li>
-          <strong>Encargado del backend</strong> – techmate solutions (2022)
-          <p>creacion de API rest para techmate solutions, diseño de bases de datos y la logica del proyecto.</p>
-        </li>
+        {experiencias.map((exp, index) => (
+          <li key={index}>
+            <strong>{exp.cargo}</strong> en {exp.empresa} ({exp.periodo})
+          </li>
+        ))}
       </ul>
     </section>
   );
 }
+export default Experiencia;
